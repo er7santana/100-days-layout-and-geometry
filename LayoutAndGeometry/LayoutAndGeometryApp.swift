0a1842a8de74ec24cad1d47487e81e557f103713
Eliezer Sant Ana <eliezer.ana@f1rst.com.br>
2024-08-14 07:59:08 -0300

    Initial Commit

---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Eliezer Rodrigo Beltramin de Sant Ana on 14/08/24.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
